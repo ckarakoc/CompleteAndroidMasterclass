056129bb4d14279e6e4bf12000895d755aa5a910
Celal Karakoç <c.karakoc@live.nl>
2025-11-29 19:38:57 +0100

    MyShoppingListApp Complete.

---
+++ MyShoppingListApp/app/src/main/java/nl/ckarakoc/myshoppinglistapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package nl.ckarakoc.myshoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import nl.ckarakoc.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyShoppingListAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ShoppingListApp()
+                }
+            }
+        }
+    }
+}
