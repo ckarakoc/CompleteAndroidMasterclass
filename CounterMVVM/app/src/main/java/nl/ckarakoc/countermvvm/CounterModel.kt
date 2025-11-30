c19e69ef9f1f18c6a8045cb5149b94cf424234c2
Celal Karakoç <c.karakoc@live.nl>
2025-11-30 10:40:20 +0100

    CounterMVVM Complete.

---
+++ CounterMVVM/app/src/main/java/nl/ckarakoc/countermvvm/CounterModel.kt
@@ -0,0 +1,21 @@
+package nl.ckarakoc.countermvvm
+
+data class CounterModel(var count: Int)
+
+class CounterRepository {
+    private var _counter = CounterModel(0)
+
+    fun getCounter() = _counter
+
+    fun incrementCounter() {
+        _counter.count++
+    }
+
+    fun decrementCounter() {
+        _counter.count--
+    }
+
+    fun resetCounter() {
+        _counter.count = 0
+    }
+}
---
+++ CounterMVVM/app/src/main/java/nl/ckarakoc/countermvvm/CounterViewModel.kt
@@ -0,0 +1,28 @@
+package nl.ckarakoc.countermvvm
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel() : ViewModel() {
+    private val repository: CounterRepository =
+        CounterRepository() // normally, you would have this in the constructor
+    private val _count = mutableStateOf(repository.getCounter().count)
+
+    val count: MutableState<Int> = _count
+
+    fun increment() {
+        repository.incrementCounter()
+        _count.value = repository.getCounter().count
+    }
+
+    fun decrement() {
+        repository.decrementCounter()
+        _count.value = repository.getCounter().count
+    }
+
+    fun reset() {
+        repository.resetCounter()
+        _count.value = repository.getCounter().count
+    }
+}
